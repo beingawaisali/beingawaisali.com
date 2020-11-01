cd094b967fe737251674cae71d3a41d203260cc9
Awais Ali <48981710+beingawaisali@users.noreply.github.com>
2020-11-01 15:27:08 +0500

    Add files via upload
    Header Created

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+// Components
+import Header from './Components/Header/Header.jsx';
+
+// Routes
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* Header */}
+
+      <Header />
+
+      {/* Body */}
+
+      {/* Footer */}
+      
+    </div>
+  );
+}
+
+export default App;
