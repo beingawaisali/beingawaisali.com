7559129c653ef11ff5c69f73e3754544161d2939
Awais Ali <48981710+beingawaisali@users.noreply.github.com>
2020-11-15 13:31:32 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import './App.css';
+import { useState } from 'react';
 
 // Components
 import Header from './Components/Header/Header.jsx';
+import Breadcrumb from './Components/Header/Breadcrumb.jsx';
+import MenuBar from './Components/Header/MenuBar.jsx';
+
+import {useTransition, animated} from 'react-spring'
 
 // Routes
 
 function App() {
+
+  let [switchMenu, setSwitchMenu] = useState(false);
+
+  const transitions = useTransition(switchMenu, null, {
+    from: { opacity: 0, trasform: 'translateX(100%)' },
+    enter: { opacity: 1, transform: 'translateX(0%' },
+    leave: { opacity: 0, translateX: 'translateX(100%)' },
+  })
+
   return (
     <div className="App">
 
       {/* Header */}
 
-      <Header />
+      <Header
+        showMenu={ () => setSwitchMenu( switchMenu == false ? true : false ) } 
+        // Enable MenuIconSwitch !!!
+        hideMenu={ () => setSwitchMenu(false) }
+        showMobileManu={ () => setSwitchMenu( switchMenu == false ? true : false ) }
+      />
+      <Breadcrumb />
+      {
+        // switchMenu == true ?  : null
+
+        transitions.map(({ item, key, props }) =>
+          item && <animated.div 
+                    key={key} 
+                    style={props}
+          >
+            <MenuBar />
+          </animated.div>
+        )
+      }
 
       {/* Body */}
 
